756070c94e4636ad16051dd52b1368d314584cfd
Chau Nguyen <chautnguyen96@gmail.com>
2017-08-02 00:25:51 -0700

    Add Summoner typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,27 +3,39 @@ declare module 'kindred-api' {
         constructor({
             key,
             region,
-            limits,
+            limits
         }: KindredConstructor)
 
-        Summoner: {
+        public Summoner: {
             get: ({
                 name,
+                id,
+                accountId
             }: {
+                id?: number,
                 name?: string,
-            }, cb: Callback) => void,
-        }
+                accountId?: number
+            }, cb?: Callback): Promise<any>,
+
+            by: {
+                id: (id: number, cb?: Callback): Promise<any>,
+                name: (name: string, cb?: Callback): Promise<any>,
+                account: (accountId: number, cb?: Callback): Promise<any>
+            }
+        };
     }
+
+    declare function print: Callback;
 }
 
 type KindredConstructor = {
     key: string,
     region?: Region,
-    limits?: Limits,
-}
+    limits?: Limits
+};
 
-type Region = 'br' | 'eune' | 'euw' | 'kr' | 'lan' | 'las' | 'na' | 'oce' | 'ru' | 'tr' | 'jp'
+type Region = 'br' | 'eune' | 'euw' | 'kr' | 'lan' | 'las' | 'na' | 'oce' | 'ru' | 'tr' | 'jp';
 
-type Limits = Array<Array<number>> // bad type
+type Limits = (number[])[]; // bad type
 
-type Callback = (err: any, data: any) => void
+type Callback = (err: any, data: any) => void;
--- typings/kindred-api.test.ts
+++ typings/kindred-api.test.ts
@@ -1,20 +1,31 @@
 /// <reference path='index.d.ts' />
+/// <reference path="./node_modules/@types/node/index.d.ts" />
 
 import * as lolapi from 'kindred-api';
 require('dotenv').config({ path: '../.env' })
 
+const key: string = process.env.KEY ? process.env.KEY as string : 'dummy'
+
 const k = new lolapi.Kindred({
-    key: process.env.KEY,
+    key,
     // limits: lolapi.LIMITS.PROD
 });
 
-k.Summoner.get({ name: "Contractz" }, function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
+k.Summoner.get({ name: "Contractz" }, lolapi.print)
+
+k.Summoner.get({ id: 32932398 }, lolapi.print)
+k.Summoner.get({ name: 'Contractz' })
+ .then((data: any) => console.log(data))
+ .catch((error: any) => console.error(error))
+
+k.Summoner.by.name('Contractz', lolapi.print)
+k.Summoner.by.name('Contractz')
+ .then((data: any) => console.log(data))
+ .catch((error: any) => console.error(error))
+try {
+    async () => {
+        const summoner = await k.Summoner.by.name('Contractz')
     }
-})
-
-
-console.log(k)
+} catch (ex) {
+    console.error(ex);
+}
