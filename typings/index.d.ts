d362c9ff746e754583b67d4adc68324aae1846b8
Chau Nguyen <chautnguyen96@gmail.com>
2017-08-02 18:02:38 -0700

    Add ChampionMastery/Champion. Fix old endpoint types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,38 +4,139 @@ declare module 'kindred-api' {
             key,
             region,
             limits
-        }: KindredConstructor)
+        }: KindredConstructor);
 
-        public Summoner: {
+        public ChampionMastery: {
             get: ({
-                name,
+                playerId,
+                championId,
+                region
+            }: {
+                playerId: number,
+                championId: number,
+                region?: Region
+            }, cb?: Callback) => Promise<ChampionMastery>
+
+            all: ({
                 id,
-                accountId
+                name,
+                accountId,
+                region
             }: {
                 id?: number,
                 name?: string,
-                accountId?: number
-            }, cb?: Callback) => Promise<any>,
+                accountId?: number,
+                region?: Region
+            }, cb?: Callback) => Promise<Array<ChampionMastery>>
+
+            totalScore: ({
+                id,
+                name,
+                accountId,
+                region
+            }: {
+                id?: number,
+                name?: string,
+                accountId?: number,
+                region?: Region
+            }, cb?: Callback) => Promise<number>
+        }
+
+        public Champion: {
+            all: ({
+                region,
+                options
+            }: {
+                region?: Region,
+                options?: {
+                    freeToPlay?: boolean
+                }
+            }, cb?: Callback) => Promise<Champions>;
 
             by: {
-                id: (id: number, cb?: Callback) => Promise<any>,
-                name: (name: string, cb?: Callback) => Promise<any>,
-                account: (accountId: number, cb?: Callback) => Promise<any>
+                id: (id: number, regionOrCallback?: Region | Callback, cb?: Callback) => Promise<Champion>
+            }
+        }
+
+        public Summoner: {
+            get: ({
+                id,
+                name,
+                accountId,
+                region
+            }: {
+                id?: number,
+                name?: string,
+                accountId?: number,
+                region?: Region
+            }, cb?: Callback) => Promise<Summoner>,
+
+            by: {
+                id: (id: number, regionOrCallback?: Region | Callback, cb?: Callback) => Promise<Summoner>,
+                name: (name: string, regionOrCallback?: Region | Callback, cb?: Callback) => Promise<Summoner>,
+                account: (accountId: number, regionOrCallback?: Region | Callback, cb?: Callback) => Promise<Summoner>
             }
         };
     }
 
     declare function print: Callback;
+    interface REGIONS {
+        BRAZIL: Region;
+        EUROPE: Region;
+        EUROPE_WEST: Region;
+        KOREA: Region;
+        LATIN_AMERICAN_NORTH: Region;
+        LATIN_AMERICA_SOUTH: Region;
+        NORTH_AMERICA: Region;
+        OCEANIA: Region;
+        RUSSIA: Region;
+        TURKEY: Region;
+        JAPAN: Region;
+    }
+
+    declare interface KindredConstructor {
+        key: string,
+        region?: Region,
+        limits?: Limits
+    };
+
+    type Region = 'br' | 'eune' | 'euw' | 'kr' | 'lan' | 'las' | 'na' | 'oce' | 'ru' | 'tr' | 'jp';
+
+    type Limits = (number[])[]; // bad type
+
+    type Callback = (err: any, data: any) => void;
+
+    class Summoner {
+        public accountId: number;
+        public id: number;
+        public name: string;
+        public summonerLevel: number;
+        public profileIconId: number;
+        public revisionDate: Date;
+    }
+
+    class ChampionMastery {
+        public championLevel: number;
+        public chestGranted: boolean;
+        public championPoints: number;
+        public championPointsSinceLastLevel: number;
+        public playerId: number;
+        public championPointsUntilNextLevel: number;
+        public tokensEarned: number;
+        public championId: number;
+        public lastPlayTime: number;
+    }
+
+    class Champions { // Riot nested this in "champions"
+        public champions: Array<Champion>;
+    }
+
+    class Champion {
+        public rankedPlayEnabled: boolean;
+        public botEnabled: boolean;
+        public botMmEnabled: boolean;
+        public active: boolean;
+        public freeToPlay: boolean;
+        public id: number;
+    }
 }
-
-type KindredConstructor = {
-    key: string,
-    region?: Region,
-    limits?: Limits
-};
-
-type Region = 'br' | 'eune' | 'euw' | 'kr' | 'lan' | 'las' | 'na' | 'oce' | 'ru' | 'tr' | 'jp';
-
-type Limits = (number[])[]; // bad type
-
-type Callback = (err: any, data: any) => void;
--- typings/kindred-api.test.ts
+++ typings/kindred-api.test.ts
@@ -3,7 +3,7 @@
 
 import * as lolapi from 'kindred-api';
 require('dotenv').config({ path: '../.env' })
-
+// const REGIONS = lolapi.REGIONS;
 const key: string = process.env.KEY ? process.env.KEY as string : 'dummy'
 
 const k = new lolapi.Kindred({
@@ -11,21 +11,95 @@ const k = new lolapi.Kindred({
     // limits: lolapi.LIMITS.PROD
 });
 
+function printSummoner(summoner: lolapi.Summoner): void {
+    console.log(
+        `
+            name                : ${summoner.name}
+            account id          : ${summoner.accountId}
+            id                  : ${summoner.id}
+            profile icon id     : ${summoner.profileIconId}
+            last revision date  : ${summoner.revisionDate}
+        `
+    )
+}
+
+function printIdsFromChampionMasteries(championMasteries: Array<lolapi.ChampionMastery>): void {
+    const championMasteriesIds = championMasteries.map(el => el.championId)
+    const concatedIds = championMasteriesIds.reduce((acc, el) => `${acc} ${el}`, '')
+    console.log(`champion masteries ids: ${concatedIds}`)
+}
+
+function printIdsFromChampions(data: lolapi.Champions): void {
+    const championIds = data.champions.map(el => el.id)
+    const concatedIds = championIds.reduce((acc, el) => `${acc} ${el}`, '')
+    console.log(`champion ids: ${concatedIds}`)
+}
+
+function printChampionMastery(championMastery: lolapi.ChampionMastery): void { // purposefully printing less info here
+    console.log(
+        `
+            playerId        : ${championMastery.playerId}
+            championLevel   : ${championMastery.championLevel}
+            championId      : ${championMastery.championId}
+
+        `
+    )
+}
+
 k.Summoner.get({ name: "Contractz" }, lolapi.print)
+k.Summoner.get({ name: "Contractz" })
+    .then(summoner => {
+        let contractz = summoner
+        printSummoner(contractz)
+    })
+    .catch(err => console.log(err))
 
 k.Summoner.get({ id: 32932398 }, lolapi.print)
 k.Summoner.get({ name: 'Contractz' })
- .then((data: any) => console.log(data))
- .catch((error: any) => console.error(error))
+ .then((data) => console.log(data))
+ .catch((error) => console.error(error))
 
 k.Summoner.by.name('Contractz', lolapi.print)
-k.Summoner.by.name('Contractz')
- .then((data: any) => console.log(data))
- .catch((error: any) => console.error(error))
-try {
-    async () => {
-        const summoner = await k.Summoner.by.name('Contractz')
-    }
-} catch (ex) {
-    console.error(ex);
-}
+
+;(async () => {
+    console.log('async print')
+    const summoner = await k.Summoner.by.name('Contractz')
+    printSummoner(summoner)
+})()
+
+;(async () => {
+    printIdsFromChampionMasteries(await k.ChampionMastery.all({ name: 'Contractz' }))
+})()
+
+k.ChampionMastery.all({ name: 'Contractz' })
+    .then(data => printIdsFromChampionMasteries(data))
+    .catch(err => console.error(err))
+
+// this is ugly since it's two required params, but I wanted to stay consistent when writing the .get functions
+k.ChampionMastery.get({ playerId: 32932398, championId: 79 })
+    .then(data => printChampionMastery(data))
+    .catch(err => console.error(err))
+
+k.Summoner.by.name('Contractz', 'na')
+
+;(async () => {
+    console.log(`Total score: ${await k.ChampionMastery.totalScore({ name: 'Contractz' })}`)
+})()
+
+;(async () => {
+    const data = await k.Champion.all({}) // hax
+    const region = 'kr'
+    const options = { freeToPlay: true }
+    const dataWithOpts = await k.Champion.all({ region, options })
+    printIdsFromChampions(data)
+    printIdsFromChampions(dataWithOpts)
+})()
+
+k.Champion.by.id(37, function (err, data) {
+    console.log(data) // we lose type data in callbacks atm
+})
+
+;(async () => {
+    const randomChampion2 = await k.Champion.by.id(37)
+    console.log(randomChampion2.botEnabled)
+})()
