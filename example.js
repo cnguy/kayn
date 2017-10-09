07565e2c637b71f5802e464f4ad3bcaf1d74bef0
cnguy <chautnguyen96@gmail.com>
2017-10-09 13:57:57 -0700

    Implement ChampionMastery API

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ const kayn = require('./dist')();
 const print = val => console.log(val);
 
 const main = async () => {
+  /*
   const res1 = await kayn.Summoner.by
     .name('Faker')
     // temporary queries, Summoner should not be able
@@ -58,9 +59,49 @@ const main = async () => {
   kayn.Summoner.by
     .name('Contractz')
     .then(summoner =>
-      kayn.Matchlist.by.accountID(summoner.accountId).query({ queue: 420 }),
+      kayn.Matchlist.get(summoner.accountId).query({ queue: 420 }),
     )
     .then(matchlist => console.log('here is the matchlist:', matchlist))
+    .catch(print);
+
+  kayn.Match.get(2472380616).callback((err, data) => {
+    if (err) {
+      console.log('why is there an error?', err);
+    } else {
+      console.log('here are the details of match:', 2472380616);
+      console.log(data);
+    }
+  });
+
+  try {
+    const timeline = await kayn.Match.timeline(2472380616);
+    console.log(timeline);
+  } catch (ex) {
+    console.log('timeline exception:', ex);
+  }
+  */
+
+  kayn.ChampionMastery.total(118341).callback((err, res) => {
+    console.log(err, res);
+  });
+
+  kayn.Summoner.by
+    .name('Contractz')
+    .region('na')
+    .callback(async (err, res) => {
+      if (res) {
+        const championIDs = [1, 2, 3, 50];
+        const cmGetter = kayn.ChampionMastery.get(res.id);
+        const mapper = async id => await cmGetter(id);
+        const championMasteries = await Promise.all(championIDs.map(mapper));
+        console.log(championMasteries);
+      }
+    });
+
+  kayn.Summoner.by
+    .name('Contractz')
+    .then(({ id }) => kayn.ChampionMastery.list(id))
+    .then(list => console.log(list.length))
     .catch(print);
 };
 
