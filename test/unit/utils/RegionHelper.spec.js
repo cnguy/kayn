fe3598c92736ac3dbedc05c74d1710ed349b1d92
cnguy <chautnguyen96@gmail.com>
2017-12-07 20:44:40 -0800

    refactor (RegionHelper): Cache region keys and re-use new functoin

--- test/unit/utils/RegionHelper.spec.js
+++ test/unit/utils/RegionHelper.spec.js
@@ -7,6 +7,8 @@ describe('RegionHelper', function() {
     describe('asPlatformID', function() {
         it('should return respective platform ID', function() {
             expect(asPlatformID('na')).to.equal('na1');
+            expect(asPlatformID('euw')).to.equal('euw1');
+            expect(asPlatformID('lan')).to.equal('la1');
         });
     });
 
@@ -17,6 +19,7 @@ describe('RegionHelper', function() {
 
         it('should return false', function() {
             expect(isValidRegion('euwsucks')).to.be.false;
+            expect(isValidRegion('na1')).to.be.false;
         });
     });
 });
