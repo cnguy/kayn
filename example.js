6e6825a2de7da71d20b2d0ad733d36f918317d59
cnguy <chautnguyen96@gmail.com>
2017-12-12 16:43:54 -0800

    feat: Experimenting with an abstraction

--- example.js
+++ example.js
@@ -1,35 +1,46 @@
 const recipes = require('./recipes');
-const { Kayn, REGIONS, METHOD_NAMES, BasicJSCache, RedisCache } = require('./');
+const {
+    Kayn,
+    Rhaast,
+    REGIONS,
+    METHOD_NAMES,
+    BasicJSCache,
+    RedisCache,
+} = require('./');
 
 const kayn = Kayn(/* optional key */)({
-  region: 'na',
-  debugOptions: {
-    isEnabled: true,
-    showKey: false,
-  },
-  requestOptions: {
-    shouldRetry: true,
-    numberOfRetriesBeforeAbort: 3,
-    delayBeforeRetry: 1000,
-    burst: true,
-  },
-  cacheOptions: {
-    cache: new RedisCache(),
-    ttls: { [METHOD_NAMES.SUMMONER.GET_BY_SUMMONER_NAME]: 1000 },
-  },
+    region: 'na',
+    debugOptions: {
+        isEnabled: true,
+        showKey: true,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: true,
+    },
+    cacheOptions: {
+        cache: new BasicJSCache(),
+        ttls: { [METHOD_NAMES.SUMMONER.GET_BY_SUMMONER_NAME]: 1000 },
+    },
 });
 
 const print = val => console.log(val);
 const printBoth = (err, data) => console.log(err, data);
 
+const Summoner = Rhaast(kayn).Summoner;
+
 const main = async () => {
-  /*
-  recipes.grabSpecificChampionScores(kayn);
-  recipes.sortChallengerLeagueByNumberOfWins(kayn);
-  recipes.grabMatchesByChampionsFromRankedMatchlist(kayn);
-  recipes.grabCurrentGameInfoOfFeaturedGamesList(kayn);
-  recipes.grabRunesAndMasteriesOfChallengerPlayers(kayn);
-  */
+    // A shell that simply has a name property and helper functions.
+    const summoner = Summoner({ name: 'Contractz' });
+
+    // The actual summoner object by Riot API.
+    const fullSummoner = await summoner.get();
+
+    // Getting the matchlist with the same summoner helper above. :)
+    // Notice how you're able to call this without any mention of the account ID.
+    const matchlist = await summoner.matchlist();
 };
 
 main();
