b210896a5e05280e49fbed361ff96cd3e7fc1ad6
cnguy <chautnguyen96@gmail.com>
2017-12-22 19:42:31 -0800

    feat (Loggers): Create basic loggers implementation

--- example.js
+++ example.js
@@ -2,28 +2,54 @@ const recipes = require('./recipes');
 const { Kayn, REGIONS, METHOD_NAMES, BasicJSCache, RedisCache } = require('./');
 
 const kayn = Kayn(/* optional key */)({
-  region: 'na',
-  debugOptions: {
-    isEnabled: true,
-    showKey: false,
-  },
-  requestOptions: {
-    shouldRetry: true,
-    numberOfRetriesBeforeAbort: 3,
-    delayBeforeRetry: 1000,
-    burst: true,
-  },
-  cacheOptions: {
-    cache: new RedisCache(),
-    ttls: { [METHOD_NAMES.SUMMONER.GET_BY_SUMMONER_NAME]: 1000 },
-  },
+    region: 'na',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: true,
+    },
+    cacheOptions: {
+        cache: new BasicJSCache(),
+        ttls: { [METHOD_NAMES.SUMMONER.GET_BY_SUMMONER_NAME]: 1000 },
+    },
 });
 
 const print = val => console.log(val);
 const printBoth = (err, data) => console.log(err, data);
 
 const main = async () => {
-  /*
+    /*
+    try {
+        const summoner = await kayn.Summoner.by.name('pyang');
+    } catch (ex) {
+        console.log(ex);
+    }
+    const summoner = await kayn.Summoner.by.name('pyang');*/
+    await kayn.Summoner.by.name('pyang');
+    for (let i = 0; i < 100; ++i)
+        kayn.Summoner.by.name('pyang').then(data => data);
+    /*
+    try {
+        return await kayn.Match.get(63506234);
+    } catch (ex) {
+        console.log('oopsie');
+    }*/
+    /*
+    kayn.Summoner.by
+        .name('Richelle')
+        .then(({ accountId }) =>
+            kayn.Matchlist.by.accountID(accountId).query({
+                endTime: 1501072595895,
+            }),
+        )
+        .then(({ matches }) => console.log(matches.length))
+        .catch(console.log);*/
+    /*
   recipes.grabSpecificChampionScores(kayn);
   recipes.sortChallengerLeagueByNumberOfWins(kayn);
   recipes.grabMatchesByChampionsFromRankedMatchlist(kayn);
