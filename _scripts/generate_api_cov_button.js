c5d0fab6030839b154ed957140ec1214bbb0d49d
cnguy <chautnguyen96@gmail.com>
2018-01-05 09:28:23 -0800

    refactor (API Cov): Add GET/POST/PUT/etc detection for more accurate coverage

--- _scripts/generate_api_cov_button.js
+++ _scripts/generate_api_cov_button.js
@@ -11,9 +11,9 @@ const PICTURES_PATH = path.join(__dirname, '..', '_pictures')
 const API_COV_SVG_BTN_PATH = path.join(PICTURES_PATH, 'api_cov.svg')
 
 const getBadgePath = percentage =>
-    `https://img.shields.io/badge/API_coverage-${encodeURI(percentage)}-9C27B0.svg`
+    `https://img.shields.io/badge/API_Coverage-${encodeURI(percentage)}-9C27B0.svg`
 const roundToHundredths = percentage => Math.round(100 * percentage) / 100
-
+const flatten = array => array.reduce((total, curr) => total.concat(curr), [])
 const processSvg = async () => {
     await svgToPng.convert(API_COV_SVG_BTN_PATH, PICTURES_PATH)
     fs.unlink(API_COV_SVG_BTN_PATH, err => {
@@ -24,16 +24,25 @@ const processSvg = async () => {
 
 request(SWAGGER_URL, async (err, res) => {
     if (res) {
-        const paths = Object.keys(JSON.parse(res.body).paths)
-        const results = await Promise.all(paths.map(async path =>
-            await findInFiles.find(`\`${path}\``, ENDPOINTS_DIR_PATH, '.js')
-        ))
-        const value = results.reduce((total, curr) =>
-            Object.keys(curr).length === 1 
-                ? total + curr[Object.keys(curr)[0]].count // TODO: Implement GET/POST/etc detection so this can be === 1. 
-                : total
+        const { paths } = JSON.parse(res.body)
+        const pathsKeys = Object.keys(paths)
+
+        const resultsPromises = Promise.all(pathsKeys.map(async pathKey => {
+            const methodTypes = Object.keys(paths[pathKey])
+            const tmp = await Promise.all(methodTypes.map(async k => {
+                const targetEndpoint = `${k.toUpperCase()} \`${pathKey}\``
+                const f = await findInFiles.find(targetEndpoint, ENDPOINTS_DIR_PATH, '.js')
+                return Object.keys(f).length > 0
+            }))
+            return tmp
+        }))
+
+        const results = flatten(await resultsPromises)
+
+        const total = results.reduce((total, curr) =>
+           curr ? total + 1 : total
         , 0)
-        const percentage = roundToHundredths(value / results.length)
+        const percentage = roundToHundredths(total / results.length)
         const readablePercentage = `${percentage * 100}%`
         const badgePath = getBadgePath(readablePercentage)
 
