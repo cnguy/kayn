3db626b19887f592105ffbce215d8d15fd3dc5d8
Chau <ChauTNguyen96@gmail.com>
2018-03-12 19:34:34 -0700

    fix (Examples): Add non-batch get matches example since totalGames is broken

--- examples/grabbing-all-the-match-dtos-of-a-player.js
+++ examples/grabbing-all-the-match-dtos-of-a-player.js
@@ -6,8 +6,33 @@ const matchlistToMatches = ({ matches }) => matches
 const matchToGameId = ({ gameId }) => gameId
 
 // getFN is required because of this file structure where `kayn` isn't included.
-// mutationsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+// Here we cannot do the calls in batch because totalGames is inaccurate.
+// So the strategy instead is to make calls while incrementing beginIndex/endIndex
+// until you get a response where the length of the matches is 0.
 const getAllMatchIDs = async (matchlistDTO, accountID, getFn) => {
+    const { totalGames } = matchlistDTO
+    console.log(`Total number of games to process: ${totalGames}`)
+
+    let rest = []
+    if (totalGames > 100) {
+        for (let beginIndex = 100; beginIndex < totalGames; beginIndex += 100) {
+            const endIndex = beginIndex + 100
+            const indexQuery = { beginIndex, endIndex }
+            const matchlist = await getFn(accountID, indexQuery)
+            if (matchlist.matches.length === 0) break // this is key
+            rest = rest.concat(matchlist)
+        }
+    }
+
+    return [matchlistDTO]
+        .concat(rest)
+        .map(matchlistToMatches)
+        .map(matchToGameId)
+}
+
+// THIS DOES NOT WORK because totalGames is inaccurate for people with
+// large amounts of games.
+const getAllMatchIDsInBatch = async (matchlistDTO, accountID, getFn) => {
     const { totalGames } = matchlistDTO
     console.log(`Total number of games to process: ${totalGames}`)
 
@@ -33,10 +58,10 @@ const main = async kayn => {
         kayn.Matchlist.by
             .accountID(accountID)
             .region('na')
-            .query({ queue: 420, season: 9 })
+            .query({ queue: 420, season: 11 })
             .query(indexQuery || {})
 
-    const { accountId: accountID } = await kayn.Summoner.by.name('Contractz')
+    const { accountId: accountID } = await kayn.Summoner.by.name('scrubnoob')
 
     // First DTO to get the total number of games.
     const firstMatchlistDTO = await getRankedMatchesForSummoner(accountID)
