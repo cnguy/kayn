bec1c8d0b7350f8f14c889bf6f974f70b66c0059
Chau <ChauTNguyen96@gmail.com>
2019-01-13 06:17:59 -0800

    docs (Examples): Minor cleanup

--- examples/async.await/v4/get-detailed-info-from-last-5-ranked-matches.js
+++ examples/async.await/v4/get-detailed-info-from-last-5-ranked-matches.js
@@ -1,7 +1,7 @@
 const processMatch = (championIdMap, summonerId, match) => {
-    const participantId = match.participantIdentities.find(
+    const { participantId } = match.participantIdentities.find(
         pi => pi.player.summonerId === summonerId,
-    ).participantId
+    )
     const participant = match.participants.find(
         p => p.participantId === participantId,
     )
@@ -23,58 +23,49 @@ const main = async kayn => {
         .accountID(accountId)
         .query({ queue: 420 })
     const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId)
-    const matchDTOS = await Promise.all(gameIds.map(kayn.MatchV4.get))
+    const matchDtos = await Promise.all(gameIds.map(kayn.MatchV4.get))
+    // `processor` is a helper function to make the subsequent `map` cleaner.
     const processor = match => processMatch(championIdMap, id, match)
-    const results = await Promise.all(matchDTOS.map(processor))
+    const results = await Promise.all(matchDtos.map(processor))
     console.log(results)
     /* may look like this =>
-[ { id: 2927354323,
-    gameCreation: 1544249052770,
+[ { gameCreation: 1544249052770,
     seasonId: 11,
     didWin: true,
     championName: 'Talon' },
-  { id: 2927307437,
-    gameCreation: 1544245697414,
+  { gameCreation: 1544245697414,
     seasonId: 11,
     didWin: false,
     championName: 'Zed' },
-  { id: 2927279380,
-    gameCreation: 1544244002044,
+  { gameCreation: 1544244002044,
     seasonId: 11,
     didWin: false,
     championName: 'Talon' },
-  { id: 2927229107,
-    gameCreation: 1544241775356,
+  { gameCreation: 1544241775356,
     seasonId: 11,
     didWin: true,
     championName: 'Talon' },
-  { id: 2916317387,
-    gameCreation: 1542980219253,
+  { gameCreation: 1542980219253,
     seasonId: 11,
     didWin: true,
     championName: 'Talon' },
-  { id: 2916325989,
-    gameCreation: 1542978026353,
+  { gameCreation: 1542978026353,
     seasonId: 11,
     didWin: false,
     championName: 'Zed' },
-  { id: 2916334457,
-    gameCreation: 1542976177203,
+  { gameCreation: 1542976177203,
     seasonId: 11,
     didWin: true,
     championName: 'Talon' },
-  { id: 2915760392,
-    gameCreation: 1542889504566,
+  { gameCreation: 1542889504566,
     seasonId: 11,
     didWin: true,
     championName: 'Talon' },
-  { id: 2915718895,
-    gameCreation: 1542887191822,
+  { gameCreation: 1542887191822,
     seasonId: 11,
     didWin: false,
     championName: 'Zed' },
-  { id: 2915727550,
-    gameCreation: 1542885211670,
+  { gameCreation: 1542885211670,
     seasonId: 11,
     didWin: false,
     championName: 'Malzahar' } ]
