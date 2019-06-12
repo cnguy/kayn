aba05133cfb7b41b0fab6bba0daf82af25af7bc8
Chau <ChauTNguyen96@gmail.com>
2019-06-12 11:45:54 -0700

    feat (LeagueEntries): Add new LeagueEntries endpoint #68 #66

--- example.js
+++ example.js
@@ -36,7 +36,14 @@ const kayn = Kayn()({
 import run from './examples/async.await/v4/get-last-10-ranked-matches-efficiently'
 
 const main = async () => {
+    /*
     console.log(await kayn.Summoner.by.name('Contractz'))
+    console.log(
+        await kayn.League.Entries.list('RANKED_SOLO_5x5', 'DIAMOND', 'I'),
+    )
+    */
+    const leagueId = 'c6fedb10-6e54-11e9-982e-c81f66cf2333'
+    console.log(await kayn.League.by.uuid(leagueId))
 }
 
 main()
---
+++ test/unit/endpoints/LeagueEndpoint/LeagueEntriesEndpointV4.spec.js
@@ -0,0 +1,70 @@
+import { expect, should, assert } from 'chai'
+
+import TestUtils from '../../../TestUtils'
+const { kaynInstance, defaultConfig } = TestUtils
+
+const { kayn, REGIONS, METHOD_TYPES } = kaynInstance
+import LeagueEntriesEndpointV4 from '../../../../lib/Endpoints/LeagueEndpoint/LeagueEntriesEndpointV4'
+import mocks from '../../../mocks'
+
+describe('LeagueEntriesEndpointV4', function() {
+    this.timeout(0)
+
+    beforeEach(function() {
+        this.LeagueEntries = new LeagueEntriesEndpointV4(defaultConfig)
+    })
+
+    describe('.by.summonerID', function() {
+        it('should have the correct payload #1', function() {
+            const { payload } = this.LeagueEntries.by.summonerID('abcdefgh')
+            expect(payload).to.deep.equal({
+                method: 'GET',
+                serviceName: 'league',
+                endpoint: 'entries/by-summoner/abcdefgh',
+                query: [],
+                region: '',
+                isTournament: false,
+                version: 4,
+            })
+        })
+
+        it('should have the correct method name', function() {
+            const { methodName } = this.LeagueEntries.by
+                .summonerID('abcdefgh')
+                .region('na')
+            expect(methodName).to.equal(
+                'LEAGUE.GET_LEAGUE_ENTRIES_BY_SUMMONER_ID_V4',
+            )
+        })
+    })
+
+    describe('.list', function() {
+        it('should have the correct payload #1', function() {
+            const { payload } = this.LeagueEntries.list(
+                'RANKED_SOLO_5x5',
+                'DIAMOND',
+                'I',
+            ).region('kr')
+            expect(payload).to.deep.equal({
+                method: 'GET',
+                serviceName: 'league',
+                endpoint: 'entries/RANKED_SOLO_5x5/DIAMOND/I',
+                query: [],
+                region: 'kr',
+                isTournament: false,
+                version: 4,
+            })
+        })
+
+        it('should have the correct method name', function() {
+            const { methodName } = this.LeagueEntries.list(
+                'RANKED_SOLO_5x5',
+                'DIAMOND',
+                'I',
+            ).region('na')
+            expect(methodName).to.equal(
+                'LEAGUE.GET_LEAGUE_ENTRIES_BY_QUEUE_TIER_DIVISION_V4',
+            )
+        })
+    })
+})
